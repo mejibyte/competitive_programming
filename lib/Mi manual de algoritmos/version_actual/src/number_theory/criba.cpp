2b641ead3974f7b3400de45e9b4ca7171010d472
Andrés Mejía <andmej@gmail.com>
2012-01-17 22:44:17 -0200

    Faster Erathostenes' sieve.

--- lib/Mi manual de algoritmos/version_actual/src/number_theory/criba.cpp
+++ lib/Mi manual de algoritmos/version_actual/src/number_theory/criba.cpp
@@ -1,20 +1,30 @@
-const int SIZE = 1000000;
+// Complejidad: O(n)
 
-//criba[i] = false si i es primo
-bool criba[SIZE+1];
+const int LIMIT = 31622779;
 
-void buildCriba(){
-  memset(criba, false, sizeof(criba));
+int sieve[LIMIT + 1]; // Inicializar con 0's.
+int primes[LIMIT + 1];
 
-  criba[0] = criba[1] = true;
-  for (int i=4; i<=SIZE; i += 2){
-    criba[i] = true;
+int primeCount = 1;
+for (int i = 2; i <= LIMIT; ++i) {
+  if (!sieve[i]) {
+    primes[primeCount] = i;
+    sieve[i] = primeCount;
+    primeCount++;
   }
-  for (int i=3; i*i<=SIZE; i += 2){
-    if (!criba[i]){
-      for (int j=i*i; j<=SIZE; j += i){
-        criba[j] = true;
-      }
-    }
+
+  for (int j = 1; j <= sieve[i] && i * primes[j] <= LIMIT; j++){
+    sieve[ i * primes[j] ] = j;
   }
 }
+
+// primes contiene la lista de primos <= LIMIT, en los índices
+// 1 a primeCount.
+// ¡El primer primo está en la posición 1 y no 0!
+
+// sieve[i] contiene el índice en el arreglo primes del primo
+// más pequeño  que divide a i. Con esta información se puede
+// saber si un número es primo o descomponerlo en primos si es
+// compuesto.
+
+// i es primo si primes[sieve[i]] == i, y compuesto si no.
