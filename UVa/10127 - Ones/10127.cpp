f41b1ab319584440d2ef79a0e7fc82b4c284e135
Andrés Mejía <andmej@gmail.com>
2012-03-27 22:42:39 -0500

    10127 - Ones from UVa

---
+++ UVa/10127 - Ones/10127.cpp
@@ -0,0 +1,48 @@
+// Andrés Mejía
+using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+////////////// Prewritten code follows. Look down for solution. ////////////////
+#define foreach(x, v) for (typeof (v).begin() x=(v).begin(); x !=(v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define D(x) cout << #x " is " << (x) << endl
+
+const double EPS = 1e-9;
+int cmp(double x, double y = 0, double tol = EPS) {
+    return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+////////////////////////// Solution starts below. //////////////////////////////
+
+int main(){
+    int n;
+    while (cin >> n) {
+        
+        int v = 1;
+        int len = 1;
+        while (v % n != 0) {
+            v = (10 * v + 1) % n;
+            len++;
+        }
+        cout << len << endl;
+    }
+    return 0;
+}
