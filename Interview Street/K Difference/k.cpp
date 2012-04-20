c6156585f324a44205ca8296ec9d01eac1ea9d43
Andrés Mejía <andmej@gmail.com>
2012-04-20 12:38:13 -0500

    Easy problem from Interview Street.

---
+++ Interview Street/K Difference/k.cpp
@@ -0,0 +1,49 @@
+// Andrés Mejía
+using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <bitset>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+////////////// Prewritten code follows. Look down for solution. ////////////////
+#define foreach(x, v) for (typeof (v).begin() x=(v).begin(); x !=(v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define D(x) cout << #x " is " << (x) << endl
+
+const double EPS = 1e-9;
+int cmp(double x, double y = 0, double tol = EPS) {
+    return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+////////////////////////// Solution starts below. //////////////////////////////
+
+const int MAXN = 100005;
+
+int a[MAXN];
+
+int main(){
+    int n, k; cin >> n >> k;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    sort(a, a + n);
+    int ans = 0;
+    //a - b = k -> b = a - k
+    for (int i = 0; i < n; ++i) ans += binary_search(a, a + n, a[i] - k);
+    cout << ans << endl;
+    return 0;
+}
