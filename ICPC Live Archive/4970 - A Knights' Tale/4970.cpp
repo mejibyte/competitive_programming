61970f6d2d887c656a1f187417b741557ff23ff2
Andrés Mejía <andmej@gmail.com>
2012-11-05 14:12:19 -0500

    Bug

--- ICPC Live Archive/4970 - A Knights' Tale/4970.cpp
+++ ICPC Live Archive/4970 - A Knights' Tale/4970.cpp
@@ -91,24 +91,12 @@ struct MCMF {
 };
 
 long long knight_distance(long long x1, long long y1, long long x2, long long y2) {
-    long long x = abs(x2 - x1), y = abs(y2 - y1);
-    // if (y > x) swap(y, x);
-    // assert(y <= x);
-    // if (y == 0 and x == 1) return 3;
-    // if (y == 2 and x == 2) return 4;
-    // 
-    // long long d = x - y;
-    // if (y > d) {
-    //     return 2 * ((y - d) / 3) + d;
-    // } else {
-    //     return d - 2 * ((d - y) / 4);
-    // }
-    
+    long long x = abs(x2 - x1), y = abs(y2 - y1);    
     if (x > y) swap(x, y);
     assert(x <= y);
     if (x == 0 and y == 1) return 3;
     if (x == 2 and y == 2) return 4;
-    long long d = max((y + 1) / 2, (x + y + 1) / 3);
+    long long d = max((y + 1) / 2, (x + y + 2) / 3);
     if (d % 2 != (x + y) % 2) d++;
     return d;
 }
