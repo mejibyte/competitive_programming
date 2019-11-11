ed823fe31faab34b34c3a8d3364f05b4c5117650
Andrés Mejía <andmej@gmail.com>
2019-11-10 20:57:52 -0800

    Simplify

--- LeetCode/1060. Missing Element in Sorted Array/linear.cpp
+++ LeetCode/1060. Missing Element in Sorted Array/linear.cpp
@@ -4,8 +4,6 @@ public:
         nums.push_back(numeric_limits<int>::max());
         for (int i = 1; i < nums.size(); ++i) {
             int missing = nums[i] - nums[i-1] - 1;
-            if (missing == 0) continue;
-
             if (missing < k) {
                 k -= missing;
                 continue;
